93aac8972ceb53a2c73cc0f49c46327782928b92
PinkLaura <34890444+PinkLaura@users.noreply.github.com>
2019-03-25 15:39:06 +0100

    sistemato layout e sfondo

--- OnePageApp.js
+++ OnePageApp.js
@@ -1,178 +1,176 @@
- $(document).ready(function(){
-	
-	TurnInCanvas(document.getElementById("immagine1"));
-	TurnInCanvas(document.getElementById("immagine2"));
-	
-	let input = document.getElementById('fileToUpload');
-	input.addEventListener('change', submitImage);
-	
-	function submitImage(){
-		let newImage = document.getElementById("fileToUpload").files[0];
-			console.log(newImage);
-		
-		let reader  = new FileReader();
-			reader.addEventListener("load", function () {
-			document.getElementById("reset").src = reader.result;
-		}, false);
-		
-		if (newImage) {
-			reader.readAsDataURL(newImage);
-		}
-		 
-	};
+$(document).ready(function() {
+
+    TurnInCanvas(document.getElementById("immagine1"));
+    TurnInCanvas(document.getElementById("immagine2"));
+
+    let input = document.getElementById('fileToUpload');
+    input.addEventListener('change', submitImage);
+
+    function submitImage() {
+        let newImage = document.getElementById("fileToUpload").files[0];
+        console.log(newImage);
+
+        let reader = new FileReader();
+        reader.addEventListener("load", function() {
+            document.getElementById("reset").src = reader.result;
+        }, false);
+
+        if (newImage) {
+            reader.readAsDataURL(newImage);
+        }
+
+    };
 
 
-	$('#submit').click(function(e){
-		e.preventDefault();
-		
-		toSliderPage();
-		
-		resetImgs("immagine1", "immagine2");
-		
-		//prendo il valore di n e applico la logica di conversione richiesta
-		let n = $('#value').val();	
-		let cLogic = $('#conversionLogic').val();
-		n = applyConversionLogic ( n, cLogic);
-		
-		//prendo il valore di sfondo e lo imposto
-		let bColor =  $('#backgroundColor').val();
-		setBackgroundColor(bColor);
-		
-		//applico l'effetto scelto
-		let effect = $('#effect').val();
-		applyEffect(effect, n, "immagine1");
-	
-	});
-	
-	$('#back').click(function(e){
-		e.preventDefault();
-		
-		toIndexPage();
-	});
-	
-	$(document).on( 'input change', '#slider', function(){
-		
-		resetImg("immagine2");
-		
-		n = $(this).val();	
-		let cLogic = $('#conversionLogic').val();
-		n = applyConversionLogic ( n, cLogic);
-		
-		let effect = $('#effect').val();
-		applyEffect(effect, n, "immagine2");
-	});
-	
-		
-	
-		function applyEffect(effect, n, id){
-			switch(effect) {
-			  case "1":
-				RemoveRandomPixel(n, id);
-				break;
-			  case "2":
-				RemoveOrderedPixel(n, id);
-				break;
-			  case "3":
-				ChangeAlpha(n, id);
-				break;
-			  case "4":
-				SwapPixel(n, id);
-				break;
-			  case "5":
-				BlurImage(n, id);
-				break;
-				
-			  default:
-				console.log("Error");
-			}
-		}
+    $('#submit').click(function(e) {
+        e.preventDefault();
 
-		function toSliderPage(){
-			let x = document.getElementById('indexPage');
-			x.style.display = 'none';
-			let y = document.getElementById('sliderPage');
-			y.style.display = 'block';
-		}
-		
-		function toIndexPage(){
-			let y = document.getElementById('sliderPage');
-			y.style.display = 'none';
-			let x = document.getElementById('indexPage');
-			x.style.display = 'block';
-		}
+        toSliderPage();
 
-		function resetImgs(...ids){
-			for (let i = 0; i < ids.length; i++){
-				resetImg(ids[i]);
-			}
-		}
+        resetImgs("immagine1", "immagine2");
 
-		//Setta lo sfondo 
-		function setBackgroundColor (bColor){			
-			document.getElementById("sliderPage").style.backgroundColor = bColor;
-		}
-		
-		function applyConversionLogic (n, cLogic){
-			
-			if ( cLogic == "S-shaped" ){
-				n =((1)/(1 + Math.pow(Math.E, (-10 * (((n)/(100))-0.5)))))*100
-			}
-			
-			return n;
-		}
-		
-		// Trasformo l'immagine in una canvas
-		function TurnInCanvas(obj) {
-			Caman(obj, function() {
-				this.render();
-			});
-			
-		}
+        //prendo il valore di n e applico la logica di conversione richiesta
+        let n = $('#value').val();
+        let cLogic = $('#conversionLogic').val();
+        n = applyConversionLogic(n, cLogic);
 
-		function resetImg(id) {
-				
-			let reset = document.getElementById("reset");
+        //prendo il valore di sfondo e lo imposto
+        let bColor = $('#backgroundColor').val();
+        setBackgroundColor(bColor);
 
-			let img = document.getElementById(id);
-			let ctxImg = img.getContext('2d');
-			let imgData = ctxImg.getImageData(0, 0, img.width, img.height).data;
+        //applico l'effetto scelto
+        let effect = $('#effect').val();
+        applyEffect(effect, n, "immagine1");
 
-			ctxImg.drawImage(reset, 0, 0);
-		}
+    });
 
-		function RemoveRandomPixel(n, id) {
-			
-			n = 100 - n;
-			
-			let img = document.getElementById(id);
+    $('#back').click(function(e) {
+        e.preventDefault();
 
-			let canvasWidth = img.width;
-			let canvasHeight = img.height;
+        toIndexPage();
+    });
 
-			let ctx = img.getContext('2d');
-			let imageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+    $(document).on('input change', '#slider', function() {
 
-			//importo i dati salvati per riapplicare la funzione dall'inizio
-			let data = imageData.data;
+        resetImg("immagine2");
 
-			//faccio passare un pixel alla volta 
-			for (let y = 0; y < canvasHeight; ++y) {
-				for (let x = 0; x < canvasWidth; ++x) {
-					let index = (y * canvasWidth + x) * 4;
-					
-					if ((Math.random() * 100) < n) { //rendo neri i 3/10 dei pixel 
-						data[index+ 3] = 0;
-					}
-				}
-			}
+        n = $(this).val();
+        let cLogic = $('#conversionLogic').val();
+        n = applyConversionLogic(n, cLogic);
 
-			ctx.putImageData(imageData, 0, 0);
-		}
+        let effect = $('#effect').val();
+        applyEffect(effect, n, "immagine2");
+    });
 
-		function RemoveOrderedPixel(n, id) {
-		
-		n = 100 - n;
-		
+    function applyEffect(effect, n, id) {
+        switch (effect) {
+            case "1":
+                RemoveRandomPixel(n, id);
+                break;
+            case "2":
+                RemoveOrderedPixel(n, id);
+                break;
+            case "3":
+                ChangeAlpha(n, id);
+                break;
+            case "4":
+                SwapPixel(n, id);
+                break;
+            case "5":
+                BlurImage(n, id);
+                break;
+
+            default:
+                console.log("Error");
+        }
+    }
+
+    function toSliderPage() {
+        let x = document.getElementById('indexPage');
+        x.style.display = 'none';
+        let y = document.getElementById('sliderPage');
+        y.style.display = 'block';
+    }
+
+    function toIndexPage() {
+        let y = document.getElementById('sliderPage');
+        y.style.display = 'none';
+        let x = document.getElementById('indexPage');
+        x.style.display = 'block';
+        document.body.className = "container bg-white text-black";
+    }
+
+    function resetImgs(...ids) {
+        for (let i = 0; i < ids.length; i++) {
+            resetImg(ids[i]);
+        }
+    }
+
+    //Setta lo sfondo 
+    function setBackgroundColor(bColor) {
+        document.body.className = "container bg-dark text-white";
+    }
+
+    function applyConversionLogic(n, cLogic) {
+
+        if (cLogic == "S-shaped") {
+            n = ((1) / (1 + Math.pow(Math.E, (-10 * (((n) / (100)) - 0.5))))) * 100
+        }
+
+        return n;
+    }
+
+    // Trasformo l'immagine in una canvas
+    function TurnInCanvas(obj) {
+        Caman(obj, function() {
+            this.render();
+        });
+    }
+
+    function resetImg(id) {
+
+        let reset = document.getElementById("reset");
+
+        let img = document.getElementById(id);
+        let ctxImg = img.getContext('2d');
+        let imgData = ctxImg.getImageData(0, 0, img.width, img.height).data;
+
+        ctxImg.drawImage(reset, 0, 0);
+    }
+
+    function RemoveRandomPixel(n, id) {
+
+        n = 100 - n;
+
+        let img = document.getElementById(id);
+
+        let canvasWidth = img.width;
+        let canvasHeight = img.height;
+
+        let ctx = img.getContext('2d');
+        let imageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+
+        //importo i dati salvati per riapplicare la funzione dall'inizio
+        let data = imageData.data;
+
+        //faccio passare un pixel alla volta 
+        for (let y = 0; y < canvasHeight; ++y) {
+            for (let x = 0; x < canvasWidth; ++x) {
+                let index = (y * canvasWidth + x) * 4;
+
+                if ((Math.random() * 100) < n) { //rendo tresparenti i n/100 dei pixel 
+                    data[index + 3] = 0;
+                }
+            }
+        }
+
+        ctx.putImageData(imageData, 0, 0);
+    }
+
+    function RemoveOrderedPixel(n, id) {
+
+        n = 100 - n;
+
         //estraggo l'immagine iniziale
         let img = document.getElementById(id);
 
@@ -184,92 +182,92 @@
 
         //importo i dati salvati per riapplicare la funzione dall'inizio
         let data = imageData.data;
-		
-		//faccio passare un pixel alla volta 
+
+        //faccio passare un pixel alla volta 
         for (let y = 0; y < canvasHeight; ++y) {
             for (let x = 0; x < canvasWidth; ++x) {
                 let index = (y * canvasWidth + x) * 4;
                 if (index % 100 < n) { //rendo neri i 3/10 dei pixel 
-            		data[index+ 3] = 0;
-				}
+                    data[index + 3] = 0;
+                }
             }
         }
 
         ctx.putImageData(imageData, 0, 0);
-      }
+    }
 
-		function ChangeAlpha(n, id) {
-			n = (n/100)*255;
-			
-			let img = document.getElementById(id);
-			
-			let canvasWidth = img.width;
-			let canvasHeight = img.height;
+    function ChangeAlpha(n, id) {
+        n = (n / 100) * 255;
 
-			let ctx = img.getContext('2d');
-			let imageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+        let img = document.getElementById(id);
 
-			//importo i dati salvati per riapplicare la funzione dall'inizio
-			let data = imageData.data;
+        let canvasWidth = img.width;
+        let canvasHeight = img.height;
 
-			//faccio passare un pixel alla volta 
-			for (let y = 0; y < canvasHeight; ++y) {
-				for (let x = 0; x < canvasWidth; ++x) {
-					let index = (y * canvasWidth + x) * 4;
-					data[index + 3] = n;
-				}
-			}
+        let ctx = img.getContext('2d');
+        let imageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
 
-			ctx.putImageData(imageData, 0, 0);
-		}
-		
-		function SwapPixel(n, id) {
-			
-			n = 100 - n;
-			
-			let img = document.getElementById(id);
+        //importo i dati salvati per riapplicare la funzione dall'inizio
+        let data = imageData.data;
 
-			let canvasWidth = img.width;
-			let canvasHeight = img.height;
+        //faccio passare un pixel alla volta 
+        for (let y = 0; y < canvasHeight; ++y) {
+            for (let x = 0; x < canvasWidth; ++x) {
+                let index = (y * canvasWidth + x) * 4;
+                data[index + 3] = n;
+            }
+        }
 
-			let ctx = img.getContext('2d');
-			let imageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+        ctx.putImageData(imageData, 0, 0);
+    }
 
-			//importo i dati salvati per riapplicare la funzione dall'inizio
-			let data = imageData.data;
+    function SwapPixel(n, id) {
 
-			//faccio passare un pixel alla volta 
-			for (let y = 0; y < canvasHeight; y++) {
-				for (let x = 0; x < canvasWidth; x++) {
-					let index = (y * canvasWidth + x) * 4;
-					let index2 = Math.round(Math.random() * canvasWidth * canvasHeight) * 4;
-					//console.log('SwapIteration');
-					
-					
-					if ((Math.random() * 100) < n) { 
-					
-						//scambio i pixel
+        n = 100 - n;
+
+        let img = document.getElementById(id);
+
+        let canvasWidth = img.width;
+        let canvasHeight = img.height;
+
+        let ctx = img.getContext('2d');
+        let imageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+
+        //importo i dati salvati per riapplicare la funzione dall'inizio
+        let data = imageData.data;
+
+        //faccio passare un pixel alla volta 
+        for (let y = 0; y < canvasHeight; y++) {
+            for (let x = 0; x < canvasWidth; x++) {
+                let index = (y * canvasWidth + x) * 4;
+                let index2 = Math.round(Math.random() * canvasWidth * canvasHeight) * 4;
+                //console.log('SwapIteration');
+
+
+                if ((Math.random() * 100) < n) {
+
+                    //scambio i pixel
 						[data[index], data[index2]] = [data[index2], data[index]];
-						[data[index+1], data[index2+1]] = [data[index2+1], data[index+1]];
-						[data[index+2], data[index2+2]] = [data[index2+2], data[index+2]];
-											
-					}
-				}
-			}
+						[data[index + 1], data[index2 + 1]] = [data[index2 + 1], data[index + 1]];
+						[data[index + 2], data[index2 + 2]] = [data[index2 + 2], data[index + 2]];
 
-			ctx.putImageData(imageData, 0, 0);
-		
-		}
+                }
+            }
+        }
 
-		function BlurImage(n, id){
-			
-			n = 100-n;
-			let obj = document.getElementById(id);
-			Caman(obj, function(){
-				this.reloadCanvasData();
-				this.stackBlur(n);
-				this.render();
-			});
-		}
+        ctx.putImageData(imageData, 0, 0);
+
+    }
+
+    function BlurImage(n, id) {
+
+        n = 100 - n;
+        let obj = document.getElementById(id);
+        Caman(obj, function() {
+            this.reloadCanvasData();
+            this.stackBlur(n);
+            this.render();
+        });
+    }
 
 });
